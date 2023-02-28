06b2c6e01594e59c9584ca7dea470cfdd9f26d8c
alfahadarc <alfahadarc@gmail.com>
2023-02-28 14:53:44 +0600

    image added

--- components/Advancedsearch/Result.tsx
+++ components/Advancedsearch/Result.tsx
@@ -4,15 +4,15 @@ import styles from './Card.module.css'
 
 
 const cards = [
-    { id: 1, imageUrl: "https://source.unsplash.com/random", title: "Example Title 1", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-    { id: 2, imageUrl: "https://source.unsplash.com/random", title: "Example Title 2", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-    { id: 3, imageUrl: "https://source.unsplash.com/random", title: "Example Title 3", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-    { id: 4, imageUrl: "https://source.unsplash.com/random", title: "Example Title 4", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-    { id: 5, imageUrl: "https://source.unsplash.com/random", title: "Example Title 5", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-    { id: 6, imageUrl: "https://source.unsplash.com/random", title: "Example Title 6", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-    { id: 7, imageUrl: "https://source.unsplash.com/random", title: "Example Title 7", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-    { id: 8, imageUrl: "https://source.unsplash.com/random", title: "Example Title 8", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-    { id: 9, imageUrl: "https://source.unsplash.com/random", title: "Example Title 9", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
+    { id: 1, imageUrl: "https://api.lorem.space/image/house?w=150&h=150", title: "Example Title 1", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
+    { id: 2, imageUrl: "https://api.lorem.space/image/house?w=150&h=150", title: "Example Title 2", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
+    { id: 3, imageUrl: "https://api.lorem.space/image/house?w=150&h=150", title: "Example Title 3", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
+    { id: 4, imageUrl: "https://api.lorem.space/image/house?w=150&h=150", title: "Example Title 4", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
+    { id: 5, imageUrl: "https://api.lorem.space/image/house?w=150&h=150", title: "Example Title 5", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
+    { id: 6, imageUrl: "https://api.lorem.space/image/house?w=150&h=150", title: "Example Title 6", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
+    { id: 7, imageUrl: "https://api.lorem.space/image/house?w=150&h=150", title: "Example Title 7", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
+    { id: 8, imageUrl: "https://api.lorem.space/image/house?w=150&h=150", title: "Example Title 8", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
+    { id: 9, imageUrl: "https://api.lorem.space/image/house?w=150&h=150", title: "Example Title 9", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
 ];
 
 export default function SearchedProduct() {
